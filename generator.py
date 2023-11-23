810973c66d18a71fe691fff30d134d0cdeabd308
Claudio Cidade <claudiocidade@gmail.com>
2023-11-24 00:49:50 +0400

    initial commit

---
+++ generator.py
@@ -0,0 +1,45 @@
+import os
+import cv2
+import time
+import pathlib
+import numpy as np
+
+# Video properties
+frame_width = 640
+frame_height = 480
+frame_rate = 1
+video_duration = 30  # in seconds
+circle_radius = 6
+
+# Main loop
+while True:
+    start_time = int(time.time() * 1000)  # current time in milliseconds
+    fourcc = cv2.VideoWriter_fourcc(*'avc1')
+    filename = f'{start_time}.mp4'
+    video = cv2.VideoWriter(filename, fourcc, frame_rate, (frame_width, frame_height))
+    end_time = start_time + (video_duration * 1000)  # 30 seconds later
+
+    # Generate random position for the circle
+    center = (np.random.randint(0, frame_width), np.random.randint(0, frame_height))
+
+    print(f'Generating video for {video_duration} seconds: final file name {filename}')
+
+    while int(time.time() * 1000) < end_time:
+        # Create a blank image
+        frame = np.zeros((frame_height, frame_width, 3), dtype=np.uint8)
+
+        cv2.circle(frame, center, circle_radius, (0, 255, 0), -1)
+
+        # Add timestamp
+        timestamp_text = f'{int(time.time() * 1000)} ms'
+        cv2.putText(frame, timestamp_text, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+
+        # Write the frame
+        video.write(frame)
+
+        # Delay to match the frame rate
+        time.sleep(1 / frame_rate)
+
+    video.release()
+
+    pathlib.Path.rename(filename, f'stream/input.mp4')
---
+++ streamer.py
@@ -0,0 +1,59 @@
+import os
+import requests
+import subprocess
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class VideoOutputWatcher(FileSystemEventHandler):
+    def on_created(self, event):
+        print('event created', event)
+        if event.is_directory:
+            return
+        if event.src_path.endswith('.mp4'):
+            self.process_video(event.src_path)
+
+    def process_video(self, video_path):
+        print('processing video', video_path)
+        # Convert MP4 to RTSP using ffmpeg
+        rtsp_stream_url = self.convert_to_rtsp(video_path)
+        # Push to API
+        self.push_to_api(rtsp_stream_url)
+
+    def convert_to_rtsp(self, video_path):
+        # Define the RTSP stream URL (adjust according to your server setup)
+        rtsp_stream_url = "rtsp://localhost:8554/mystream"
+        # Command to use ffmpeg to stream the video to the RTSP server
+
+        ffmpeg_command = [
+            'ffmpeg',
+            '-re',
+            '-i', video_path,
+            '-c:v', 'copy',
+            '-c:a', 'copy',
+            '-f', 'rtsp',
+            rtsp_stream_url
+        ]
+
+        # Start the ffmpeg process in the background
+        subprocess.Popen(ffmpeg_command)
+
+        # Return the RTSP stream URL
+        return rtsp_stream_url
+
+def main():
+    path = "stream/"
+    event_handler = VideoOutputWatcher()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=False)
+    observer.start()
+    try:
+        print('Watching for new videos...')
+        while True:
+            # Run indefinitely
+            pass
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+if __name__ == "__main__":
+    main()
